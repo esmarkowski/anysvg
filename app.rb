938dff1030ef092823e7fd3cb0618d69ba96e283
Spencer Markowski <spencer.markowski@gmail.com>
2024-05-20 07:08:14 -0400

    Add cache

--- app.rb
+++ app.rb
@@ -9,37 +9,54 @@ class App < Sinatra::Base
     logger = Logger.new(STDOUT)
     STDOUT.sync = true
     set :logger, logger
+    set :redis, Redis.new(url: ENV['CACHETOGO_URL'])
   end
 
   get '/:service/:size/:icon' do
-    size = params['size'].to_i || 16
-    service = params['service']
-    icon = params['icon']
-    color = params['color']
+    cache(request.url) do
+      size = params['size'].to_i || 16
+      service = params['service']
+      icon = params['icon']
+      color = params['color']
 
-    case service
-    when 'si'
-      url = "https://simpleicons.org/icons/#{icon}.svg"
-    when 'gh'
-      # icon_size if size is less 24 use 16 else use 24 unless icon_size parameter is provided
-      icon_size = size < 24 ? 16 : 24
-      icon_size = params['icon_size'].to_i if params['icon_size']
-      url = "https://raw.githubusercontent.com/primer/octicons/main/icons/#{icon}-#{icon_size}.svg"
-    when 'bs'
-      url = "https://icons.getbootstrap.com/assets/icons/#{icon}.svg"
-    else
-      return status 400
+      case service
+      when 'si'
+        url = "https://simpleicons.org/icons/#{icon}.svg"
+      when 'gh'
+        # icon_size if size is less 24 use 16 else use 24 unless icon_size parameter is provided
+        icon_size = size < 24 ? 16 : 24
+        icon_size = params['icon_size'].to_i if params['icon_size']
+        url = "https://raw.githubusercontent.com/primer/octicons/main/icons/#{icon}-#{icon_size}.svg"
+      when 'bs'
+        url = "https://icons.getbootstrap.com/assets/icons/#{icon}.svg"
+      else
+        return status 400
+      end
+
+      generate_svg(url, size, color)
     end
-
-    generate_svg(url, size, color)
   end
 
   get '/icon' do
-    size = params['size'].to_i
-    url = params['url']
-    color = params['color']
+    cache(request.url) do
+      size = params['size'].to_i
+      url = params['url']
+      color = params['color']
 
-    generate_svg(url, size, color)
+      generate_svg(url, size, color)
+    end
+  end
+
+  def cache(key)
+    cached_response = settings.redis.get(key)
+
+    if cached_response
+      cached_response
+    else
+      response = yield
+      settings.redis.set(key, response)
+      response
+    end
   end
 
   private
